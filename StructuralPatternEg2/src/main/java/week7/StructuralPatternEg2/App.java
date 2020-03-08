accf829fa180feffa963de82634004aaba43017e
DELL <rajdeepmajumder925@gmail.com>
2020-03-09 00:18:57 +0530

    Cmmitting week7.4

---
+++ StructuralPatternEg2/src/main/java/week7/StructuralPatternEg2/App.java
@@ -0,0 +1,74 @@
+package week7.StructuralPatternEg2;
+import java.util.ArrayList;    //Implementing Composite design patttern
+import java.util.List;
+
+abstract class Account {
+  public abstract float getBalance();
+}
+
+class DepositeAccount extends Account {
+  private String accountNo;
+  private float accountBalance;
+
+  public DepositeAccount(String accountNo, float accountBalance) {
+    super();
+    this.accountNo = accountNo;
+    this.accountBalance = accountBalance;
+  }
+
+  public float getBalance() {
+    return accountBalance;
+  }
+
+}
+
+class SavingAccount extends Account {
+  private String accountNo;
+  private float accountBalance;
+
+  public SavingAccount(String accountNo, float accountBalance) {
+    super();
+    this.accountNo = accountNo;
+    this.accountBalance = accountBalance;
+  }
+
+  public float getBalance() {
+    return accountBalance;
+  }
+}
+
+class CompositeAccount extends Account {
+  private float totalBalance;
+  private List<Account> accountList = new ArrayList<Account>();
+
+  public float getBalance() {
+    totalBalance = 0;
+    for (Account f : accountList) {
+      totalBalance = totalBalance + f.getBalance();
+    }
+    return totalBalance;
+  }
+
+  public void addAccount(Account acc) {
+    accountList.add(acc);
+  }
+
+  public void removeAccount(Account acc) {
+    accountList.remove(acc);
+  }
+}
+
+public class App {
+
+  public static void main(String[] args) {
+    CompositeAccount component = new CompositeAccount();
+
+    component.addAccount(new DepositeAccount("DA001", 100));
+    component.addAccount(new DepositeAccount("DA002", 150));
+    component.addAccount(new SavingAccount("SA001", 200));
+
+    float totalBalance = component.getBalance();
+    System.out.println("Total Balance : " + totalBalance);
+  }
+
+}
